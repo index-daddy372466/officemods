9c1b68e2dadd720b6e2cb5fa3f00102711d13101
KylesTech95 <108553476+KylesTech95@users.noreply.github.com>
2024-10-16 19:47:18 -0400

    fork: module list item hover

--- client/public/main.js
+++ client/public/main.js
@@ -1,6 +1,33 @@
-const title = document.querySelector('.title-header')
+const title = document.querySelector(".title-header");
+const playbtns = document.querySelectorAll(".fl-right");
+const module_list = document.querySelectorAll(".modules-list-item");
 // fade in title
-window.onload = e => {
-    title.classList.remove('hidden')
-    title.classList.add('appear')
-}
+window.onload = (e) => {
+  title.classList.remove("hidden");
+  title.classList.add("appear");
+};
+
+// lis mouseover
+let currentMod
+module_list.forEach((li, idx) => {
+    // if btn-container matches the play button (in same index)
+  if (li.children[1] == playbtns[idx].parentElement) {
+    currentMod = li;
+    currentMod.onmouseenter = (e) => {
+      const play = playbtns[idx].parentElement || e.target.children[1];
+      play.classList.add("white-border");
+      play.onmouseenter = (e) => {
+        play.classList.remove("white-border");
+        play.classList.add("green-border");
+      };
+      play.onmouseleave = (e) => {
+        play.classList.add("white-border");
+        play.classList.remove("green-border");
+      };
+    };
+    currentMod.onmouseleave = (e) => {
+      const play = playbtns[idx].parentElement || e.target.children[1];
+      play.classList.remove("white-border");
+    };
+  }
+});
