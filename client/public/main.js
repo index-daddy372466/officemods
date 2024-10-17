9c20c952b68fc7e0ced8fc48f0823496e574ce2b
KylesTech95 <108553476+KylesTech95@users.noreply.github.com>
2024-10-17 17:43:58 -0400

    feat: arrows added

--- client/public/main.js
+++ client/public/main.js
@@ -4,24 +4,31 @@ const module_list = document.querySelectorAll(".modules-list-item");
 const preview = document.getElementById('preview')
 // const img = preview.children[0] || document.querySelector('.prev-img')
 const img = document.querySelector('.prev-img')
+const prevMessage = document.getElementById('message')
 // fade in title
 window.onload = (e) => {
   title.classList.remove("hidden");
   title.classList.add("appear");
 };
 
-// interact with each module
+// interact with each module on left side
 let currentMod
 module_list.forEach((li, idx) => {
     // if btn-container matches the play button (in same index)
-  if (li.children[1].children[0] == playbtns[idx].parentElement) {
+  if (li.children[2].children[0] == playbtns[idx].parentElement) {
+    // assign li to currentMod
     currentMod = li;
+    // mouse enter event listener
     currentMod.onmouseenter = (e) => {
       playbtns[idx].classList.remove('fl-right')
+      prevMessage.classList.add('hide-prev')
+      prevMessage.classList.remove('show-prev-msg')
       img.classList.remove('hide-prev')
       let mod = e.currentTarget;
       const text = mod.children[0].textContent.toLowerCase();
       const play = playbtns[idx].parentElement || mod.children[1];
+      const arrow = mod.children[1]
+      arrow.classList.add('clip-arrow')
       play.classList.add("white-border");
       // while inside li, play-button can be modified
       play.onmouseenter = (e) => {
@@ -40,7 +47,13 @@ module_list.forEach((li, idx) => {
       img.src = url;
       img.alt = text;
     };
+    // mouse leave event listener
     currentMod.onmouseleave = (e) => {
+      let mod = e.currentTarget;
+      const arrow = mod.children[1]
+      arrow.classList.remove('clip-arrow')
+        prevMessage.classList.remove('hide-prev')
+        prevMessage.classList.add('show-prev-msg')
         const play = playbtns[idx].parentElement || e.target.children[1];
         play.classList.remove("white-border");
         img.classList.add('hide-prev')
