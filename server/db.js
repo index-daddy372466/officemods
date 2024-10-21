9150afdb2323535d1b37c6cdf3177505cd5aabff
KylesTech95 <108553476+KylesTech95@users.noreply.github.com>
2024-10-21 10:06:53 -0400

    test: sequelize databases

--- server/db.js
+++ server/db.js
@@ -1,35 +1,43 @@
 require('dotenv').config()
 const { Pool } = require('pg')
-// const { Sequelize } = require('sequelize')
+const { Sequelize } = require('sequelize')
 
-    // const sequalize = new Sequelize(process.env.URI,
-    //     {dialect:"sqlite",
-    //      storage:"./database.sqlite",
-    //      logging:false,
-    //     })
-    //     sequalize.sync().then(()=>{
-    //         console.log('you are connected to pg')
-    //     }).catch(err=>console.log(err))
+    const sequalize = new Sequelize(process.env.N_URI,
+        {dialect:"sqlite",
+         storage:"./database.sqlite",
+         logging:false,
+        })
+        sequalize.sync().then(()=>{
+            console.log('Sequelize: you are connected to notesDB')
+        }).catch(err=>console.log(err))
+        const sequalize2 = new Sequelize(process.env.C_URI,
+            {dialect:"sqlite",
+             storage:"./database.sqlite",
+             logging:false,
+            })
+            sequalize2.sync().then(()=>{
+                console.log('Sequelize: you are connected to calculatorDB')
+            }).catch(err=>console.log(err))
 const pool = {
     notepad: new Pool({
-        user: process.env.DBU,
-        database: process.env.DB,
-        password: process.env.PD,
-        port: process.env.DBP,
-        host:process.env.DBH,
-        // ssl:{
-        //     rejectUnauthorized:false,
-        // }
+        user: process.env.N_DBU,
+        database: process.env.N_DB,
+        password: process.env.N_PD,
+        port: process.env.N_DBP,
+        host:process.env.N_DBH,
+        ssl:{
+            rejectUnauthorized:false,
+        }
     }),
     calculator: new Pool({
-        user: process.env.DBU,
-        database: process.env.DB2,
-        password: process.env.PD,
-        port: process.env.DBP,
-        host:process.env.DBH,
-        // ssl:{
-        //     rejectUnauthorized: false,
-        // }
+        user: process.env.C_DBU,
+        database: process.env.C_DB2,
+        password: process.env.C_PD,
+        port: process.env.C_DBP,
+        host:process.env.C_DBH,
+        ssl:{
+            rejectUnauthorized:false,
+        }
     
     })
 }
